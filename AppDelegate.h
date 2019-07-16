179d48700c3deab46de7588f3f09e3b0d5322c57
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-16 10:12:02 -0600

    Initial commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quakes
+//
+//  Created by Mitchell Budge on 7/16/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
